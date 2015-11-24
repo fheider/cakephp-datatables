4bef100033e4a52acfa2a13b5d8fa4a9d9ae2f6f
Johannes Jordan <johannes.jordan@physiologie.uni-freiburg.de>
2015-11-24 11:47:22 +0100

    only initiate searches when field value changed
    prevent redundant AJAX calls and table redraws

--- webroot/js/cakephp.dataTables.js
+++ webroot/js/cakephp.dataTables.js
@@ -18,20 +18,23 @@ var delay = 600;
 
 /**
  * Add search behavior to all search fields in column footer
- *
 */
 function initSearch ()
 {
     table.api().columns().every( function () {
         var index = this.index();
+        var lastValue = ''; // closure variable to prevent redundant AJAX calls
         $('input, select', this.footer()).on('keyup change', function () {
-            // -- set search
-            table.api().column(index).search( this.value );
-            window.clearTimeout(oFilterTimerId);
-            oFilterTimerId = window.setTimeout(drawTable , delay);
+            if (this.value != lastValue) {
+                lastValue = this.value;
+                // -- set search
+                table.api().column(index).search(this.value);
+                window.clearTimeout(oFilterTimerId);
+                oFilterTimerId = window.setTimeout(drawTable, delay);
+            }
         });
     });
-};
+}
 
 /**
  * Function reset
